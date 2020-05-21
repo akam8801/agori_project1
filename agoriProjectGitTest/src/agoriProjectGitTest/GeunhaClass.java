1a5c968c35c5f3fc8b7ec6fee204724f4fee1897
CREWMATE <CREWMATE@DESKTOP-4MT55G5>
2020-05-21 16:08:23 +0900

    마스터 ㅂㅂ

--- agoriProjectGitTest/src/agoriProjectGitTest/GeunhaClass.java
+++ agoriProjectGitTest/src/agoriProjectGitTest/GeunhaClass.java
@@ -5,9 +5,10 @@ public class GeunhaClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("수정 1번입니다.");
-		System.out.println("수정 2번입니다.");
+		System.out.println("수정 3번입니다.");
 		System.out.println("부렌치를 엄청 잘 따보자1");
 		System.out.println("겹칠건데 어떻게 병합하니?");
+		System.out.println("마스터랑 다르면 어떻게 병합하니?");
 		System.out.println("브렌치 됐으니까 이번에 수정해서 마스터에 1 ");
 	}
 

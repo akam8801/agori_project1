640890dc6e7d7adbd95977fa08cfa7ee29b0ac30
CREWMATE <CREWMATE@DESKTOP-4MT55G5>
2020-05-21 14:40:33 +0900

    노푸시 커밋 2

--- agoriProjectGitTest/src/agoriProjectGitTest/GeunhaClass.java
+++ agoriProjectGitTest/src/agoriProjectGitTest/GeunhaClass.java
@@ -5,6 +5,7 @@ public class GeunhaClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("수정 1번입니다.");
+		System.out.println("수정 2번입니다.");
 	}
 
 }

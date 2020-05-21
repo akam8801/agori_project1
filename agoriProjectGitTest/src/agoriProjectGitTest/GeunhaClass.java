39e0eff80cf488dc90252350f9efacbd869a7bbf
CREWMATE <CREWMATE@DESKTOP-4MT55G5>
2020-05-21 15:05:53 +0900

    머지할 소스 커밋

--- agoriProjectGitTest/src/agoriProjectGitTest/GeunhaClass.java
+++ agoriProjectGitTest/src/agoriProjectGitTest/GeunhaClass.java
@@ -6,7 +6,8 @@ public class GeunhaClass {
 		// TODO Auto-generated method stub
 		System.out.println("수정 1번입니다.");
 		System.out.println("수정 2번입니다.");
-		System.out.println("부렌치를 따보자1");
+		System.out.println("부렌치를 엄청 잘 따보자1");
+		System.out.println("겹칠건데 어떻게 병합하니?");
 		System.out.println("브렌치 됐으니까 이번에 수정해서 마스터에 1 ");
 	}
 

db78f03cd7070330f3e6bef3debf8405f5f3ef44
CREWMATE <CREWMATE@DESKTOP-4MT55G5>
2020-05-21 14:54:36 +0900

    엥?

--- agoriProjectGitTest/src/agoriProjectGitTest/GeunhaClass.java
+++ agoriProjectGitTest/src/agoriProjectGitTest/GeunhaClass.java
@@ -7,7 +7,7 @@ public class GeunhaClass {
 		System.out.println("수정 1번입니다.");
 		System.out.println("수정 2번입니다.");
 		System.out.println("부렌치를 따보자1");
-		System.out.println("브렌치 됐으니까 이번에 수정해서 마스터에 ");
+		System.out.println("브렌치 됐으니까 이번에 수정해서 마스터에 1 ");
 	}
 
 }

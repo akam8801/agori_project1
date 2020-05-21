85b961183f5bfa66cb29109552542a1e2aabcb3d
CREWMATE <CREWMATE@DESKTOP-4MT55G5>
2020-05-21 16:17:07 +0900

    브렌치 소스

--- agoriProjectGitTest/src/agoriProjectGitTest/GeunhaClass.java
+++ agoriProjectGitTest/src/agoriProjectGitTest/GeunhaClass.java
@@ -5,9 +5,10 @@ public class GeunhaClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("수정 1번입니다.");
-		System.out.println("수정 2번입니다.");
+		System.out.println("수정 4번입니다.");
 		System.out.println("부렌치를 엄청 잘 따보자1");
 		System.out.println("겹칠건데 어떻게 병합하니?");
+		System.out.println("이건 브랜치 소스야 어떻게 병합하니?");
 		System.out.println("브렌치 됐으니까 이번에 수정해서 마스터에 1 ");
 	}
 
